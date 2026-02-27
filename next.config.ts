d8849441d69bd8574bdca7a67df3387a86e0623f
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2026-02-27 10:15:00 +0100

    FIX

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/fire',
+  basePath: process.env.CI ? '/fire' : '',
+  turbopack: {
+    root: __dirname,
+  },
 }
 
 export default nextConfig;
